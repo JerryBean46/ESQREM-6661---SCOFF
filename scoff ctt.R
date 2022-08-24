a0789e21a14e158218bf0793384ee5804ad5d59c
JerryBean46 <45081280+JerryBean46@users.noreply.github.com>
2022-08-24 09:47:59 -0400

    Add files via upload

--- scoff ctt.R
+++ scoff ctt.R
@@ -1,32 +1,36 @@
-#Set working directory
-setwd("E:/Teaching(Short)/ED 6661 - Autumn 2022/R -SCOFF")
+##Set working directory
+##setwd("E:/Teaching(Short)/ED 6661 - Autumn 2022/R -SCOFF")
 
 ##Load Libraries
 library(CTT)
 library(psych)
-library(sem)
+library(summarytools)
 
 ##Read Data
 data <- read.csv("scoff.csv", header=TRUE)
-scoff <- (data[,1:5]) #Define data
+scoff <- (data[,1:5])
+attach(scoff)
 
-##Descriptives
-sumScore <- scoff$Item.1 + scoff$Item.2 + scoff$Item.3 + scoff$Item.4 + scoff$Item.5
+##Compute a scale(sum) score
+sumScore <- rowSums(scoff)
+
+##Item and Scale Frequencies
+freq(Item.1)
+freq(Item.2)
+freq(Item.3)
+freq(Item.4)
+freq(Item.5)
+freq(sumScore)
+hist(sumScore, breaks = 5)
 describe(sumScore)
-
-scoff.tab <- table(sumScore)
-scoff.tab
-scoff.pct <- prop.table(scoff.tab)*100
-scoff.pct
 
 ##CTT Analysis
 itemAnalysis(scoff)
-
-options(digits = 3)
 iA = itemAnalysis(scoff, itemReport=TRUE, NA.Delete=TRUE)
 iA$itemReport
 
 ##Compute a standard error of measurement
 sem <- 1.18 * sqrt(1 - .669)
 sem
+
 
